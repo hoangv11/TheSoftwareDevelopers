4016984034538bbfeb5df3bf6a4283e7adb0b49c
hoangv11 <victorhoang11@gmail.com>
2024-12-01 04:00:01 -1000

    Authentication for edit session

--- src/app/editsession/page.tsx
+++ src/app/editsession/page.tsx
@@ -1,10 +1,24 @@
 import { Suspense } from 'react';
+import authOptions from '@/lib/auth';
+import { getServerSession } from 'next-auth';
 import EditSession from '@/components/EditSessionForm';
 
-export default function EditSessionPage() {
+const EditSessionPage = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user || !session.user.email) {
+    return <div>Session not found</div>;
+  }
+
+  const userSession = session as unknown as {
+    user: { email: string; id: string; randomKey: string };
+  };
+  const currentUser = parseInt(userSession.user.id, 10);
+
   return (
     <Suspense fallback="Loading...">
-      <EditSession />
+      <EditSession currentUser={currentUser} />
     </Suspense>
   );
-}
+};
+
+export default EditSessionPage;
